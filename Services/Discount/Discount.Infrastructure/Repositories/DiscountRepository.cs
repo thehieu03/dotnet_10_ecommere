76d23c90541cf0bb0f143f2fbdab669ec5d9af38
Nguyễn Thế Hiếu <153330033+thehieu03@users.noreply.github.com>
2025-11-27 03:49:00 +0700

    seed data to postgresql

--- Services/Discount/Discount.Infrastructure/Repositories/DiscountRepository.cs
+++ Services/Discount/Discount.Infrastructure/Repositories/DiscountRepository.cs
@@ -1,24 +1,12 @@
-﻿using Dapper;
-using Discount.Core.Entities;
-using Discount.Core.Repositories;
-using Microsoft.Extensions.Configuration;
-using Npgsql;
+﻿namespace Discount.Infrastructure.Repositories;
 
-namespace Discount.Infrastructure.Repositories;
-
-public class DiscountRepository : IDiscountRepository
+public class DiscountRepository(IConfiguration configuration) : IDiscountRepository
 {
-    private readonly IConfiguration _configuration;
-
-    public DiscountRepository(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
     public async Task<Coupon> GetDiscountAsync(string productName)
     {
         await using var connection =
-            new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString") ??
+                                 throw new InvalidOperationException());
         var coupon = await connection.QueryFirstAsync<Coupon>
             ("SELECT * FROM Coupon WHERE ProductName=@ProductName", new { ProductName = productName });
         if (coupon == null)
@@ -37,7 +25,8 @@ public class DiscountRepository : IDiscountRepository
     public async Task<bool> CreateDiscountAsync(Coupon coupon)
     {
         await using var connection =
-            new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString") ??
+                                 throw new InvalidOperationException());
         var affected = await connection.ExecuteAsync
         ("INSERT INTO Coupon (ProductName, Description, Amout) VALUES (@ProductName, @Description, @Amout)",
             new { ProductName = coupon.ProductName, Description = coupon.Description, Amout = coupon.Amount });
@@ -47,7 +36,8 @@ public class DiscountRepository : IDiscountRepository
     public async Task<bool> UpdateDiscountAsync(Coupon coupon)
     {
         await using var connection =
-            new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString") ??
+                                 throw new InvalidOperationException());
         var affected = await connection.ExecuteAsync
         ("UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amout=@Amout WHERE Id=@Id",
             new
@@ -61,7 +51,8 @@ public class DiscountRepository : IDiscountRepository
     public async Task<bool> DeleteDiscountAsync(string productName)
     {
         await using var connection =
-            new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString") ??
+                                 throw new InvalidOperationException());
         var affected = await connection.ExecuteAsync("DELETE FROM Coupon WHERE ProductName = @ProductName",
             new { ProductName = productName });
         return affected != 0;

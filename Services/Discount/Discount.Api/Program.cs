7c64c91f7dab0abf721894b61351439bc0aa8f16
Nguyễn Thế Hiếu <153330033+thehieu03@users.noreply.github.com>
2025-11-27 08:16:29 +0700

    update docker file folder Discount

--- Services/Discount/Discount.Api/Program.cs
+++ Services/Discount/Discount.Api/Program.cs
@@ -1,13 +1,41 @@
+using System.Reflection;
+using Discount.Api.Services;
+using Discount.Application.Handlers;
+using Discount.Core.Repositories;
+using Discount.Infrastructure.Extensions;
+using Discount.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenApi();
+// Register AutoMapper
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+// Register MediatR
+var assemblies = new Assembly[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(CreateDiscountCommandHandler).Assembly
+};
+builder.Services.AddMediatR(cfg=> cfg.RegisterServicesFromAssemblies(assemblies));
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddGrpc();
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
 }
-
-app.UseHttpsRedirection();
-
+// Migrate Database
+app.MigrateDatabase<Program>();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<DiscountServices>();
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. " +
+                                          "To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    });
+});
 
 
 app.Run();

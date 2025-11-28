d7b35dd88863f96970fe2a18ac7f7dbab9046c3b
Nguyễn Thế Hiếu <153330033+thehieu03@users.noreply.github.com>
2025-11-28 10:19:46 +0700

    update OrderRepository.cs

---
+++ Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository(OrderContext dbContext) : RepositoryBase<Order>(dbContext), IOrderRepository
+{
+    public async Task<IEnumerable<Order>> GetOrderByUserName(string username)
+    {
+        var orderList = await dbContext
+            .Orders
+            .Where(o => o.UserName == username)
+            .ToListAsync();
+        return orderList;
+    }
+}

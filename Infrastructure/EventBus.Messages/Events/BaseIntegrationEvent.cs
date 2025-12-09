28a912d44e2ad923b176e50ac071b609a8aee3ea
Nguyễn Thế Hiếu <hieunthe171211@gmail.com>
2025-12-09 11:11:38 +0700

    udpate eventbus constant

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace EventBus.Messages.Common;
+
+public class BaseIntegrationEvent
+{
+    public string CorelationId { get; set; }
+    public DateTime CreationDate { get; set; }
+    public BaseIntegrationEvent()
+    {
+        CorelationId=Guid.NewGuid().ToString();
+        CreationDate = DateTime.UtcNow;
+    }
+    public BaseIntegrationEvent(Guid correlationId,DateTime creationDate)
+    {
+        CorelationId = correlationId.ToString();
+        CreationDate = creationDate;
+    }
+}
